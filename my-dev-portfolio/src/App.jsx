f10e750fc7c82475f339563132c0c68bcc7650f5
Hogan Nguyen <hoganngu756@gmail.com>
2024-06-16 19:49:54 -0500

    Website V1

--- my-dev-portfolio/src/App.jsx
+++ my-dev-portfolio/src/App.jsx
@@ -7,8 +7,8 @@ function App() {
 
   return (
     <>
-      <div className="container mx-auto grid grid-cols-3 gap-4">
-        <div className="border border-red-500">column 1</div>
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-4 p-3 md:p-7">
+        <div className="border border-red-500 text-amber-600">column 1</div>
         <div className="border border-black col-span-2">column 2</div>
         </div>
     </>
